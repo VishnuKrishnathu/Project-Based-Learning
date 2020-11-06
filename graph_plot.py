2f9f694384dc7ac318c7fd03969cf3ac2366a091
VishnuKrishnathu <vishnu.krishnathu1999@gmail.com>
2020-11-06 12:12:08 +0530

    Create graph_plot.py

---
+++ graph_plot.py
@@ -0,0 +1,34 @@
+from scipy.integrate import odeint
+import numpy
+import matplotlib.pyplot as plt
+import math
+
+def model(x,t,K,F,m, c):
+    x1 = x[0]
+    x2 = x[1]
+    dx1_dt = x2
+    dx2_dt = (F-(c*x2)-(K*x1))/m
+    dx_dt = [dx1_dt, dx2_dt]
+    return dx_dt
+
+def main(value, number,zeta):
+    K = value[number-1].konstant
+    F = (value[number-1].mass+0.005)*9.81
+    m = value[number-1].mass
+    displacement = value[number-1].static
+    try:
+        c = zeta*2*(math.sqrt(K*m))
+    except ValueError:
+        pass
+    except ZeroDivisionError:
+        pass
+    except UnboundLocalError:
+        pass
+    time = (value[number-1].time)*10
+
+    x_1 = [displacement, 0]
+    t = numpy.linspace(0,time, 100)
+    x = odeint(model, x_1, t, args = (K, F, m, c))
+
+    plt.plot(t, x[:, 0])
+    plt.show()
