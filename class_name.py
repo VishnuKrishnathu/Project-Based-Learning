e9948c5ae820237de13292fda7a5d745d71aa96e
VishnuKrishnathu <vishnu.krishnathu1999@gmail.com>
2020-11-25 10:22:17 +0530

    Create class_name.py

---
+++ class_name.py
@@ -0,0 +1,9 @@
+class properties:
+    def __init__(self, length, mass, static, konstant, time, frequency, natural):
+        self.length = length
+        self.mass= mass
+        self.static = static
+        self.konstant = konstant
+        self.time = time
+        self.frequency = frequency
+        self.natural = natural
